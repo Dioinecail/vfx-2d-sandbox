4a26bc31a263fcb1d09847a70d2a8cdb2ae104c3
Dioinecail <andrewdionecail@gmail.com>
2020-01-15 00:34:01 +0300

    Added stuff for Breakdown on 2D

---
+++ Assets/_Scripts/CameraShake.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    private Transform cameraTransform;
+    public float Amplitude { get; set; }
+
+    private Coroutine coroutine_shake;
+
+
+
+    private void Awake()
+    {
+        cameraTransform = GetComponent<Transform>();
+    }
+
+    public void Shake(float seconds)
+    {
+        if (coroutine_shake != null)
+            StopCoroutine(coroutine_shake);
+
+        coroutine_shake = StartCoroutine(ShakeCoroutine(seconds));
+    }
+
+    private IEnumerator ShakeCoroutine(float seconds)
+    {
+        float timer = 0;
+
+        while(timer <= seconds)
+        {
+            timer += Time.deltaTime;
+
+            Vector3 randomDirection = new Vector3(Random.Range(-1, 1f), Random.Range(-1, 1f), 0);
+
+            cameraTransform.position = Vector3.Lerp(cameraTransform.position, cameraTransform.position + randomDirection * Amplitude, 0.5f);
+            yield return new WaitForSeconds((1 / 10f) * Time.timeScale);
+        }
+    }
+}
---
+++ Assets/_Scripts/OnHit2DEvent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnHit2DEvent : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public bool spawnOnCollision;
+
+    public UnityEvent onHitEvent;
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        onHitEvent?.Invoke();
+        if (spawnOnCollision)
+        {
+            int contacts = collision.contactCount;
+
+            for (int i = 0; i < contacts; i++)
+            {
+                SpawnObject(objectToSpawn, collision.GetContact(i).point);
+            }
+        }
+    }
+
+    public void SpawnObject(GameObject go, Vector3 position)
+    {
+        Instantiate(go, position, go.transform.rotation);
+    }
+}
